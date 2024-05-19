76d5bf806cb0af4944bc2478beea4d4ed051865f
JoshuaL15 <165833703+JoshuaAL15@users.noreply.github.com>
2024-05-19 02:28:52 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -85,13 +85,15 @@ Pair * searchMap(HashMap * map,  char * key) {
     }
 
     return NULL;
-
-
-    return NULL;
 }
 
 Pair * firstMap(HashMap * map) {
-
+    for (long i = 0; i < map->capacity; i++) {
+        if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+            map->current = i;
+            return map->buckets[i];
+        }
+    }
     return NULL;
 }
 
